916391f6301fd8c63a9530671d197d2e9ac8121d
Olalekan Yusuf <olasyusuf@gmail.com>
2025-09-28 11:57:52 +0100

    feat: Initialize project scaffolding

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,11 @@
+const Home: React.FC = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-gray-800">
+        Welcome to My Next.js App 
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
